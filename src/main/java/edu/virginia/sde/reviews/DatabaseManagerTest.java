1b73f294b962c057a342a124183703714cff4d83
ks5tj <ks5tj@virginia.edu>
2023-12-05 03:05:41 -0500

    testing

--- src/main/java/edu/virginia/sde/reviews/DatabaseManagerTest.java
+++ src/main/java/edu/virginia/sde/reviews/DatabaseManagerTest.java
@@ -6,13 +6,12 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +23,16 @@ public class DatabaseManagerTest {
 
     @BeforeAll
     public static void setUp() {
+
         DatabaseManager db = new DatabaseManager();
         db.initializeHibernate();
         sessionFactory = db.getSessionFactory();
     }
+    @BeforeEach
+    public void setUpEach() {
+        DatabaseManager.clearDatabase();
+    }
+
 
     @AfterAll
     public static void tearDown() {
@@ -91,48 +96,57 @@ public class DatabaseManagerTest {
 
 
     }
+
     @Test
     public void testGetReviewsByUser() {
-//        // Add users
-//        DatabaseManager.addNewUser("user1", "password1");
-//        DatabaseManager.addNewUser("user2", "password2");
-//
-//        // Add courses
-//        DatabaseManager.addCourse("CS", 101, "Intro to Computer Science", 0.0, new ArrayList<>());
-//        DatabaseManager.addCourse("MATH", 201, "Advanced Mathematics", 0.0, new ArrayList<>());
-//
-//        // Retrieve users and courses for reference
-//        User user1 = DatabaseManager.getUserByUsername("user1");
-//        Course course1 = DatabaseManager.getCourseByMnemonicAndNumber("CS", 101);
-//        Course course2 = DatabaseManager.getCourseByMnemonicAndNumber("MATH", 201);
-//
-//
-//        //Add reviews by user1
-//        LocalDateTime localDateTime1 = LocalDateTime.of(2023, 1, 1, 1, 1, 1, 1_000_000);
-//        Timestamp timestamp1 = Timestamp.valueOf(localDateTime1);
-//        LocalDateTime localDateTime2 = LocalDateTime.of(2023, 1, 2, 1, 1, 1, 1_000_000);
-//        Timestamp timestamp2 = Timestamp.valueOf(localDateTime2);
-//        DatabaseManager.addReview(user1, course1, 5, timestamp1, "Excellent course");
-//        DatabaseManager.addReview(user1, course2, 4, timestamp2, "Challenging but rewarding");
-//
-//        // Fetch reviews by user1
-//        List<Review> reviews = DatabaseManager.getReviewsByUser(user1.getId());
-//        Assertions.assertNotNull(reviews, "Review list should not be null");
-//        Assertions.assertEquals(2, reviews.size(), "User1 should have 2 reviews");
-//
-//        // Check contents of the reviews
-//        Review firstReview = reviews.get(0);
-//        Review secondReview = reviews.get(1);
-//        Assertions.assertTrue(firstReview.getComment().equals("Excellent course") || secondReview.getComment().equals("Excellent course"), "One review should be 'Excellent course'");
-//        Assertions.assertTrue(firstReview.getComment().equals("Challenging but rewarding") || secondReview.getComment().equals("Challenging but rewarding"), "One review should be 'Challenging but rewarding'");
 
+        // Ensure users and courses are unique for this test
+        String uniqueUser1 = "user1_" + System.currentTimeMillis();
+        String uniqueUser2 = "user2_" + System.currentTimeMillis();
+
+        // Add users
+        DatabaseManager.addNewUser(uniqueUser1, "password1");
+        DatabaseManager.addNewUser(uniqueUser2, "password2");
+
+        // Add courses
+        DatabaseManager.addCourse("CS", 101, "Intro to Computer Science", 0.0, new ArrayList<>());
+        DatabaseManager.addCourse("MATH", 201, "Advanced Mathematics", 0.0, new ArrayList<>());
+
+        // Retrieve users and courses for reference
+        User user1 = DatabaseManager.getUserByUsername(uniqueUser1);
+        Course course1 = DatabaseManager.getCourseByMnemonicAndNumber("CS", 101).get(0);
+        Course course2 = DatabaseManager.getCourseByMnemonicAndNumber("MATH", 201).get(0);
+
+        // Add reviews
+        DatabaseManager.addReview(uniqueUser1, course1.getCourseId(), 5, "Excellent course");
+        DatabaseManager.addReview(uniqueUser1, course2.getCourseId(), 4, "Challenging but rewarding");
+
+        // Fetch reviews by user1
+        List<Review> reviews = DatabaseManager.getReviewsByUser(user1.getId());
+        Assertions.assertNotNull(reviews, "Review list should not be null");
+        Assertions.assertEquals(2, reviews.size(), "User1 should have 2 reviews");
+
+        // Check contents of the reviews
+        boolean foundExcellentCourse = false;
+        boolean foundChallengingButRewarding = false;
+        for (Review review : reviews) {
+            if ("Excellent course".equals(review.getComment())) {
+                foundExcellentCourse = true;
+            }
+            if ("Challenging but rewarding".equals(review.getComment())) {
+                foundChallengingButRewarding = true;
+            }
+        }
+        Assertions.assertTrue(foundExcellentCourse, "One review should be 'Excellent course'");
+        Assertions.assertTrue(foundChallengingButRewarding, "One review should be 'Challenging but rewarding'");
     }
+
+
     @Test
     public void testtest() {
-        List<Course> courses = new ArrayList<>();
-        courses = DatabaseManager.getAllCourses();
-        for (Course course : courses) {
-            System.out.println(course.toString());
+        List<Review> reviews = DatabaseManager.getAllReviews();
+        for (Review review : reviews) {
+            System.out.println(review.toString());
         }
     }
     @Test
@@ -153,6 +167,120 @@ public class DatabaseManagerTest {
         // Cleanup: Optionally delete the test user after the test
         DatabaseManager.deleteUser(testUsername);
     }
+    @Test
+    public void testUpdateReview() {
+        // Ensure the database is clean
+        DatabaseManager.clearDatabase();
+
+        // Arrange: Add a user and a course
+        String username = "testUser";
+        String password = "password123";
+        DatabaseManager.addNewUser(username, password);
+
+        String mnemonic = "CS";
+        int courseNumber = 101;
+        String courseTitle = "Intro to Computer Science";
+        DatabaseManager.addCourse(mnemonic, courseNumber, courseTitle, 0.0, new ArrayList<>());
+
+        // Retrieve the course to get its ID
+        List<Course> courses = DatabaseManager.getAllCourses();
+        Assertions.assertFalse(courses.isEmpty(), "Courses should not be empty");
+        Course addedCourse = courses.get(0);
+        String courseId = addedCourse.getCourseId(); // Get the ID of the added course
+
+        // Add a review
+        int originalRating = 3;
+        String originalComment = "Good course";
+        DatabaseManager.addReview(username, courseId, originalRating, originalComment);
+
+        // Retrieve the added review to get its ID
+        List<Review> reviews = DatabaseManager.getReviewsByUser(username);
+        Assertions.assertFalse(reviews.isEmpty(), "Reviews should not be empty");
+        Review addedReview = reviews.get(0);
+        int reviewId = addedReview.getId(); // Get the ID of the added review
+
+        // Act: Update the review
+        int newRating = 4;
+        String newComment = "Great course";
+        DatabaseManager.updateReview(reviewId, newRating, newComment);
+
+        // Assert: Retrieve the review and check if it's updated
+        Review updatedReview = DatabaseManager.getReviewById(reviewId);
+        Assertions.assertEquals(newRating, updatedReview.getRating(), "Rating should be updated");
+        Assertions.assertEquals(newComment, updatedReview.getComment(), "Comment should be updated");
+    }
+
+
+    @Test
+    public void testClearDatabase() {
+        // Setup: Add some entities
+        DatabaseManager.addNewUser("testUser", "password");
+        DatabaseManager.addCourse("CS", 101, "Intro to Computer Science", 5.0, new ArrayList<>());
+        DatabaseManager.addReview("testUser", "CS101", 5, "Great course");
+
+        // Act: Clear the database
+        DatabaseManager.clearDatabase();
+
+        // Assert: Check if all tables are empty
+        List<User> users = DatabaseManager.getAllUsers();
+        List<Course> courses = DatabaseManager.getAllCourses();
+        List<Review> reviews = DatabaseManager.getAllReviews();
+
+        Assertions.assertTrue(users.isEmpty(), "Users table should be empty");
+        Assertions.assertTrue(courses.isEmpty(), "Courses table should be empty");
+        Assertions.assertTrue(reviews.isEmpty(), "Reviews table should be empty");
+    }
+    @Test
+    public void testHasUserReviewedCourse() {
+        // Ensure the database is clean
+        DatabaseManager.clearDatabase();
+
+        // Arrange: Add a user and a course
+        String username = "testUser";
+        String password = "password123";
+        DatabaseManager.addNewUser(username, password);
+
+        String mnemonic = "CS";
+        int courseNumber = 101;
+        String courseTitle = "Intro to Computer Science";
+        DatabaseManager.addCourse(mnemonic, courseNumber, courseTitle, 0.0, new ArrayList<>());
+
+        // Retrieve the course to get its ID
+        List<Course> courses = DatabaseManager.getAllCourses();
+        Assertions.assertFalse(courses.isEmpty(), "Courses should not be empty");
+        Course addedCourse = courses.get(0);
+        String courseId = addedCourse.getCourseId(); // Get the ID of the added course
+
+        // Act: Add a review
+        DatabaseManager.addReview(username, courseId, 5, "Excellent course");
+
+        // Assert: Check if the user has reviewed the course
+        boolean result = DatabaseManager.hasUserReviewedCourse(username, courseId);
+        Assertions.assertTrue(result, "User should have reviewed the course");
+
+        // Act & Assert: Check for a course the user hasn't reviewed
+        String otherCourseId = "CS102"; // Ensure this is a different course ID
+        result = DatabaseManager.hasUserReviewedCourse(username, otherCourseId);
+        Assertions.assertFalse(result, "User should not have reviewed this other course");
+    }
+
+    @Test
+    public void testDeleteReview() {
+        // Arrange: Add a review to delete
+        String username = "testUser";
+        String courseId = "CS101";
+        DatabaseManager.addReview(username, courseId, 5, "Excellent course");
+
+        // Act: Delete the review
+        int reviewId = 1; // assuming this is the ID of the review added above
+        DatabaseManager.deleteReview(reviewId);
+
+        // Assert: Check if the review is deleted
+        Review deletedReview = DatabaseManager.getReviewById(reviewId);
+        Assertions.assertNull(deletedReview, "Review should be deleted");
+    }
+
+
 }
 
 
